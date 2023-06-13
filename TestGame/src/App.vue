65a6bdaec45baffc05613b0c1e82d404bb16c32e
Seth Holmes (Laptop) <sholmes2022@gmail.com>
2023-06-12 20:40:26 -0500

    Converting main app to script setup

--- TestGame/src/App.vue
+++ TestGame/src/App.vue
@@ -1,105 +1,120 @@
 <script lang="ts">
-  import { defineComponent, onMounted } from 'vue';
-  import Header from "./components/Header.vue";
-  import EntityTile from "./components/EntityTile.vue";
-  import MonsterData from "./assets/MonsterData.json";
+import MonsterData from "./assets/MonsterData.json";
 
-  enum Environment {
-    Forest
+enum Environment {
+  Forest,
+}
+
+enum EntityType {
+  ForestGoblinFighter,
+  ForestWolfhound,
+  ForestGreatStag,
+}
+
+enum Rank {
+  Normal,
+  Veteran,
+  Elite,
+  Boss,
+}
+
+export class Enemy {
+  img: String;
+  imgAlt: String;
+  rank: Rank;
+  maxHealth: Number;
+  currentHealth: Number;
+  physicalDmg: Number;
+
+  constructor(type: EntityType) {
+    this.rank = Rank.Normal;
+    this.img = MonsterData[type].img;
+    this.imgAlt = MonsterData[type].imgAlt;
+    this.maxHealth = MonsterData[type].maxHealth;
+    this.physicalDmg = MonsterData[type].physicalDmg;
+    this.currentHealth = this.maxHealth;
   }
+}
+</script>
 
-  enum EntityType {
-    ForestGoblinFighter,
-    ForestWolfhound,
-    ForestGreatStag
-  }
+<script setup lang="ts">
+import Header from "./components/Header.vue";
+import EntityTile from "./components/EntityTile.vue";
+import { ref, onMounted } from "vue";
 
-  enum Rank {
-    Normal,
-    Veteran,
-    Elite,
-    Boss
-  }
+const count = ref(1);
+const headerButtonsDisabled = ref([false, false, true]);
+const currentEnemies = ref([Enemy, Enemy, Enemy, Enemy]);
+const tiles = ref([] as (typeof EntityTile)[]);
 
-  export class Enemy {
-    img: String;
-    imgAlt: String;
-    rank: Rank;
-    maxHealth: Number;
-    currentHealth: Number;
-    physicalDmg: Number;
+const TileOne = ref(null);
+const TileTwo = ref(null);
+const TileThree = ref(null);
+const TileFour = ref(null);
 
-    constructor(type: EntityType) {
-      this.rank = Rank.Normal;
-      this.img = MonsterData[type].img;
-      this.imgAlt = MonsterData[type].imgAlt;
-      this.maxHealth = MonsterData[type].maxHealth;
-      this.physicalDmg = MonsterData[type].physicalDmg;
-      this.currentHealth = this.maxHealth;
-    }
-  }
-
-  export default defineComponent({
-    data() {
-      return {
-        count: 1,
-        headerButtonsDisabled: [false, false, true],
-        currentEnemies: [Enemy, Enemy, Enemy, Enemy],
-        tiles: [] as any[],
-        refNames: ["TileOne", "TileTwo", "TileThree", "TileFour"]
-      }
-    },
-    methods: {
-      increment() {
-        this.tiles.forEach(tile => {
-          tile.enemy = new Enemy(EntityType.ForestGoblinFighter);
-          tile.activateTile();
-        });
-      },
-      activate() {
-        this.tiles.forEach(tile => {
-          tile.activateTile();
-        });
-      },
-      deactivate() {
-        this.tiles.forEach(tile => {
-          tile.deactivateTile();
-        });
-      }
-    },
-    components: {
-      Header,
-      EntityTile
-    },
-    mounted() {
-      this.tiles.push(this.$refs.TileOne);
-      this.tiles.push(this.$refs.TileTwo);
-      this.tiles.push(this.$refs.TileThree);
-      this.tiles.push(this.$refs.TileFour);
-      this.tiles.forEach((tile) => {
-        tile.deactivateTile();
-      });
-    }
+const increment = function () {
+  tiles.value.forEach((tile) => {
+    tile.enemy = new Enemy(EntityType.ForestGoblinFighter);
+    tile.activateTile();
   });
-  
+};
+
+const activate = function () {
+  tiles.value.forEach((tile) => {
+    tile.activateTile();
+  });
+};
+
+const deactivate = function () {
+  tiles.value.forEach((tile) => {
+    tile.deactivateTile();
+  });
+};
+
+onMounted(() => {
+  console.log(TileOne.value);
+  tiles.value.push(TileOne.value);
+  tiles.value.push(TileTwo.value);
+  tiles.value.push(TileThree.value);
+  tiles.value.push(TileFour.value);
+  tiles.value.forEach((tile) => {
+    tile.deactivateTile();
+  });
+});
 </script>
 
 <template>
-    <Header :disabledButtons="headerButtonsDisabled"/>
-    <h1>Hello {{ count }}</h1>
-    <button @click="increment()">Increment</button>
-    <button @click="activate()">Activate</button>
-    <button @click="deactivate()">Deactivate</button>
-    <EntityTile styleLeft="650px" styleTop="50px"  ref="TileOne"/>
-    <EntityTile styleLeft="1200px" styleTop="50px"  ref="TileTwo"/>
-    <EntityTile styleLeft="650px" styleTop="450px"  ref="TileThree"/>
-    <EntityTile styleLeft="1200px" styleTop="450px"  ref="TileFour"/>
+  <Header :disabledButtons="headerButtonsDisabled" />
+  <h1>Hello {{ count }}</h1>
+  <button @click="increment()">Increment</button>
+  <button @click="activate()">Activate</button>
+  <button @click="deactivate()">Deactivate</button>
+  <EntityTile
+    styleLeft="650px"
+    styleTop="50px"
+    ref="TileOne"
+  />
+  <EntityTile
+    styleLeft="1200px"
+    styleTop="50px"
+    ref="TileTwo"
+  />
+  <EntityTile
+    styleLeft="650px"
+    styleTop="450px"
+    ref="TileThree"
+  />
+  <EntityTile
+    styleLeft="1200px"
+    styleTop="450px"
+    ref="TileFour"
+  />
 </template>
 
 <style>
-  body {
-    color:white;
-    background-color: black;
-    background-image: url("/img/environments/ForestBackground.png");
-  }
+body {
+  color: white;
+  background-color: black;
+  background-image: url("/img/environments/ForestBackground.png");
+}
 </style>
--- TestGame/src/components/Header.vue
+++ TestGame/src/components/Header.vue
@@ -1,30 +1,35 @@
 <script lang="ts">
-  import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
-  export default defineComponent({
-    name: "Header",
-    data() {
-      return {
-        numOfHeaderButtons: 3,
-      }
+export default defineComponent({
+  name: "Header",
+  data() {
+    return {
+      numOfHeaderButtons: 3,
+    };
+  },
+  props: {
+    disabledButtons: {
+      type: [] as boolean[],
+      required: true,
     },
-    props: {
-      disabledButtons: {
-        type: [] as boolean[],
-        required: true
-      }
-    },
-  });
+  },
+});
 </script>
 
 <template>
   <div>
-    <button v-for="(value, key) in numOfHeaderButtons" :disabled="disabledButtons[key]" >Header Button {{ value }}</button>
+    <button
+      v-for="(value, key) in numOfHeaderButtons"
+      :disabled="disabledButtons[key]"
+    >
+      Long Header Button {{ value }}
+    </button>
   </div>
 </template>
 
 <style scoped>
-    button {
-        background-color: aqua;
-    }
+button {
+  background-color: aqua;
+}
 </style>
